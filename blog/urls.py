7d8c3badbe3451dc11e62bf2230fcaff3849ea9f
pranav <pranavkneeraj@gmail.com>
2015-06-17 13:06:16 +0530

    dsfsddfsa

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$', views.post_list),
+    url(r'^$', views.post_list),url(r'^pranav$', views.post_list),
+    url(r'^post/new/$', views.post_new, name='post_new'),url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
 ]
